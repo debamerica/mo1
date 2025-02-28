70d16d36685abb346f9136f36bebeae9c4a39df7
debamerica <devhalderamerica@gmail.com>
2025-02-28 20:28:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,20 +1,10 @@
 import streamlit as st
-import streamlit.components.v1 as components
 
-# HTML code that triggers an automatic redirect
-html_code = """
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta http-equiv="refresh" content="0; url=https://huggingface.co/spaces/DebabrataHalder/RecommendMovies" />
-  </head>
-  <body>
-    <p>If you are not redirected automatically, click 
-      <a href="https://huggingface.co/spaces/DebabrataHalder/RecommendMovies">here</a>.
-    </p>
-  </body>
-</html>
-"""
+# Define the URL to redirect to
+redirect_url = "https://huggingface.co/spaces/DebabrataHalder/RecommendMovies"
 
-# Render the HTML code in a Streamlit component
-components.html(html_code, height=0, width=0)
+# Use an HTML meta refresh tag to perform an immediate redirect
+st.markdown(
+    f'<meta http-equiv="refresh" content="0; url={redirect_url}" />',
+    unsafe_allow_html=True
+)
